dcb89a1f24a3104282673e9e1e494089b1473363
GHADEER KHASAWNEH <GHADEER_KHASAWNEH@YAHOO.COM>
2021-07-25 16:33:07 +0300

    adding components

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class Footer extends React.Component {
+    render(){
+        return(
+            <>
+            <Footer>Ghadeer Khasawneh</Footer>
+            </>
+        )
+    }
+}
+
+export default Footer
---
+++ src/Components/HornedBeasts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+class HornedBeasts extends React.Component {
+    render(){
+        return(
+            <>
+            <div className = 'threeValues'>{this.props.info}</div>
+            </>
+        )
+    }
+}
+
+export default HornedBeasts;
